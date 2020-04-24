54ca92e58e8c0fbfa19a78eb3480b8aecfa09396
zakhodgson1 <zhodgson@uwo.ca>
2020-04-24 13:17:46 -0400

    added scripts for auth

--- app/src/main/java/com/example/smashpractice/LoginActivity.java
+++ app/src/main/java/com/example/smashpractice/LoginActivity.java
@@ -2,13 +2,37 @@ package com.example.smashpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.InjectView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @InjectView(R.id.emailInput) EditText emailInput;
+    @InjectView(R.id.pwInput) EditText passwordInput;
+    @InjectView(R.id.loginButton) Button loginButton;
+    @InjectView(R.id.signupLink) TextView signupLink;
+    @InjectView(R.id.pwLink) TextView pwLink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signupLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/smashpractice/SignupActivity.java
+++ app/src/main/java/com/example/smashpractice/SignupActivity.java
@@ -2,13 +2,39 @@ package com.example.smashpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.InjectView;
 
 public class SignupActivity extends AppCompatActivity {
+
+    @InjectView(R.id.Gtag) EditText username;
+    @InjectView(R.id.emailInput) EditText emailInput;
+    @InjectView(R.id.pwInput) EditText passwordInput;
+    @InjectView(R.id.loginB) Button loginButton;
+    @InjectView(R.id.signupB) Button signupButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Login activity
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
