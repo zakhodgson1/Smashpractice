5665c504b67bd40b476070a2a56f155ec529eb59
zakhodgson1 <zhodgson@uwo.ca>
2020-04-24 12:59:55 -0400

    Set up stitch db, Templates for login/signup done

--- app/src/main/java/com/example/smashpractice/MainActivity.java
+++ app/src/main/java/com/example/smashpractice/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.smashpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.mongodb.stitch.android.core.Stitch;
+import com.mongodb.stitch.android.core.StitchAppClient;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.loginB)
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize stitch client
+        final StitchAppClient client = Stitch.initializeDefaultAppClient("smashpractice-jxlen");
+
+        final RemoteMongoClient mongoClient = client.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
